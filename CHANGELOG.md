69d7f909bed9fac2b3530e525d4acc705e553ad8
Jakub Biesiada <biesiadajakub@icloud.com>
2018-03-29 09:22:33 +0200

    v1.0.0 beta 3
    - removed shadow options
    - added callbacks on mouse enter, move, leave and device move

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,22 +1,26 @@
 # Changelog
 
+## 1.0.0 beta 3
+* **removed shadow options**
+* **added callbacks on mouse enter, move, leave and device move**
+
 ## 1.0.0 beta 2
 * updated README.md
 * updated demo page
 * code cleaned up
-* added autoinit
+* **added autoinit**
 * fixed performance of shine effect
 * fixed performance on mobile devices
 
 ## 1.0.0 beta 1
 * npm added
 * AMD & CommonJS support added
-* redesigned demo page (new header view, dark theme)
+* **redesigned demo page (new header view, dark theme)**
 * updated README.md
-* added shadow-color option
-* renamed jQuery plugin from UniversalTilt() to universalTilt()
+* added shadow-color option (**Removed in 1.0.0 beta 3**)
+* **renamed jQuery plugin from UniversalTilt() to universalTilt()**
 
 ## 0.5.0
-* Position Base option
-* mobile support (devices with gyroscope)
+* **Position Base option**
+* **mobile support (devices with gyroscope)**
 * plugin works in vanillaJS as well as jQuery
--- demo/js/script.js
+++ demo/js/script.js
@@ -1,13 +1,23 @@
 const tilts = document.querySelectorAll('.tilt');
 const liveTilt = new UniversalTilt(tilts, {
-	'shadow-color': 'rgba(255, 255, 255, 0.2)'
+	onMouseEnter: el => {
+		el.style.boxShadow = '0 45px 100px rgba(255, 255, 255, 0.3)';
+	},
+
+	onMouseLeave: el => {
+		el.style.boxShadow = '0 45px 100px rgba(255, 255, 255, 0)';
+	},
+
+	onDeviceMove: el => {
+		el.style.boxShadow = '0 45px 100px rgba(255, 255, 255, 0.3)';
+	}
 });
 
-let eventBox = document.querySelector("#event");
-let outputContainer = document.querySelector(".output");
+let eventBox = document.querySelector('#event');
+let outputContainer = document.querySelector('.output');
 
-eventBox.addEventListener("tiltChange", (event) => {
-	let li = document.createElement("li");
+eventBox.addEventListener('tiltChange', event => {
+	let li = document.createElement('li');
 	li.innerHTML = `<strong>X</strong>: ${event.detail.tiltX} | <strong>Y</strong>: ${event.detail.tiltY}`;
 	outputContainer.insertBefore(li, outputContainer.firstChild);
 });
