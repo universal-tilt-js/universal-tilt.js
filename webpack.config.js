985f83b450b31eeed181b065957c26a8b5c0cbcb
Jakub Biesiada <biesiadajakub@icloud.com>
2018-07-09 21:31:28 +0200

    Gulp -> Webpack & Babel v6 -> v7
    + switched from Gulp to Webpack
    + updated Babel to v7
    + now only 2 versions (with Babel): minified (now default) and unminified and maps
    + demo moved to gh-pages branch

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env;
+
+let libraryName = 'universal-tilt';
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
+}
+
+const config = {
+  mode: mode,
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: 'UniversalTilt',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  }
+};
+
+module.exports = config;
