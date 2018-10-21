57bc2a99a7080630b9c31271e040fa40b0458fcb
Jakub Biesiada <biesiadajakub@icloud.com>
2018-10-21 20:33:25 +0200

    Updates

---
+++ precache-manifest.9e4d84b43759f9dbb3404b7653aaccb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c915b3eca929189ea875",
+    "url": "./static/js/main.c915b3ec.chunk.js"
+  },
+  {
+    "revision": "6e103cb493e42a672c3d",
+    "url": "./static/js/1.6e103cb4.chunk.js"
+  },
+  {
+    "revision": "c915b3eca929189ea875",
+    "url": "./static/css/main.f324d969.chunk.css"
+  },
+  {
+    "revision": "3582200809acbc3eb54b60a10552adbb",
+    "url": "./index.html"
+  }
+];
