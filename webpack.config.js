b7a45a3ac5f79cf5611337a25a2b5609cfeaabcc
Jakub Biesiada <biesiadajakub@icloud.com>
2018-11-20 20:35:44 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const path = require('path');
 const env = require('yargs').argv.env;
 
-let libraryName = 'universal-tilt';
+const libraryName = 'universal-tilt';
 
 let outputFile, mode, devtool;
 
