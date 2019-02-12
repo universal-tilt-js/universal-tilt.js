28386989da1999ce3b533ad572b61b4312ad599e
Jakub Biesiada <biesiadajakub@icloud.com>
2019-02-12 22:14:28 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,39 @@
-const env = require('yargs').argv.env;
+module.exports = env => {
+  const libraryName = 'universal-tilt';
 
-const libraryName = 'universal-tilt';
+  let outputFile, mode, devtool;
 
-let outputFile, mode, devtool;
-
-if (env === 'build') {
-  mode = 'production';
-  outputFile = `${libraryName}.min.js`;
-  devtool = false;
-} else {
-  mode = 'development';
-  outputFile = `${libraryName}.js`;
-  devtool = 'source-map';
-}
-
-const config = {
-  mode,
-  entry: `${__dirname}/src/${libraryName}.js`,
-  devtool,
-  output: {
-    path: `${__dirname}/lib`,
-    filename: outputFile,
-    library: 'UniversalTilt',
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-    globalObject: 'typeof window !== "object" ? global.window = global : window'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
+  if (env === 'build') {
+    mode = 'production';
+    outputFile = `${libraryName}.min.js`;
+    devtool = false;
+  } else {
+    mode = 'development';
+    outputFile = `${libraryName}.js`;
+    devtool = 'source-map';
   }
-};
 
-module.exports = config;
+  return {
+    mode,
+    entry: `${__dirname}/src/${libraryName}.js`,
+    devtool,
+    output: {
+      path: `${__dirname}/lib`,
+      filename: outputFile,
+      library: 'UniversalTilt',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+      globalObject:
+        'typeof window !== "object" ? global.window = global : window'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    }
+  };
+};
