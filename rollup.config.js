8465718d2e012240977f2fc0ce5af359b2856db5
Jakub Biesiada <biesiadajakub@icloud.com>
2020-01-17 20:43:42 +0100

    Replace Webpack with Rollup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [resolve(), typescript(), terser()],
+  external: Object.keys(pkg.dependencies)
+};
